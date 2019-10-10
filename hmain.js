d2d720467415f425c405667bd544060486700d71
Luke Horgan <horgan.l@husky.neu.edu>
2019-10-10 18:14:57 -0400

    ahahah

--- hmain.js
+++ hmain.js
@@ -1,91 +1,43 @@
 const readline = require('readline');
 const fs = require('fs');
-const sqlite3 = require('sqlite3');
+var sizeof = require('object-sizeof');
 
 function main() {
     let args = process.argv.slice(2);
     let csvFilename = args[0];
-    let dbFilename = args[1];
-    createDatabase(csvFilename, dbFilename);
+    read(csvFilename);
 }
 
-async function createDatabase(inputFilename, dbFilename) {
-    /*let db = new sqlite3.Database(dbFilename);
-    db.run('CREATE TABLE `urls` (`og` TEXT,`can` TEXT, `error` INTEGER, `complete` INTEGER);', (err) => {
-        if(err) {
-            console.log("Could not create database");
-            console.log(err);
-        }
-        else {
-            let lineReader = readline.createInterface({
-                input: fs.createReadStream(inputFilename)
-            });
-        
-            lineReader.on('line', (line) => {
-                //console.log(line);
-                let url = line.trim();
-                db.run('INSERT INTO urls(og) VALUES(?)', [url], (err) => {
-                    if(err) {
-                        console.log("error inserting " + url);
-                    }
-                    else {
-                        console.log("Successfully insterted url " + url);
-                    }
-                });
-            });
-        }
-    });*/
-
-    //let db = new DB(dbFilename);
-    let csv = new CSV(inputFilename);
+function read(inputFilename) {
+    const readInterface = readline.createInterface({
+        input: fs.createReadStream(inputFilename),
+    });
     
-    let i = 0;
-    do {
-        var nextLine = await csv.readline();
-        if(i % 100 === 0) {
-            console.log("Hi");
+    let urls = [];
+
+    readInterface.on('line', function(line) {
+        let url = line.trim();
+        urls.push(url);
+    });
+
+    readInterface.on('close', () => {
+        console.log("done");
+        console.log(sizeof(urls));
+    });
+}
+
+class Earl {
+    constructor() {
+        this.workers = this.makeWorkers();
+    }
+
+    makeWorkers() {
+        let workers = [];
+        for(let i = 0; i < os.cpus().length; i++) {
+            let worker = new Worker(path, { workerData });
+            workers.push(worker);
         }
-        //console.log(i + ": " + nextLine);
-        i++;
-    } while(nextLine);
-}
-
-class CSV {
-    constructor(filename) {
-        this.lineReader = readline.createInterface({
-            input: fs.createReadStream(filename)
-        }); 
-    }
-
-    async readline() {
-        return new Promise((resolve) => {
-            this.lineReader.on('line', (line) => {
-                resolve(line.trim());
-            });
-        });
-    }
-}
-
-class DB {
-    constructor(dbFilename) {
-        this.db = new sqlite3.Database(dbFilename);
-    }
-
-    async run(sql, params) {
-        return new Promise((resolve, reject) => {
-            db.run(sql, params, (err) => {
-                if(err) {
-                    reject(err);
-                }
-                else {
-                    resolve();
-                }
-            });
-        });
-    }
-
-    close() {
-        this.db.close();
+        return workers;
     }
 }
 
