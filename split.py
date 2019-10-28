586aa37b6c09a8a1420c905356142a94391092bf
Luke Horgan <horgan.l@husky.neu.edu>
2019-10-28 15:26:13 -0400

    update split

--- split.py
+++ split.py
@@ -1,6 +1,8 @@
 import os
 import random
 
+from csv import reader
+
 def split(filename, results_folder, file_num):
     if not os.path.exists(results_folder):
         os.makedirs(results_folder)
@@ -24,10 +26,46 @@ def split(filename, results_folder, file_num):
     
     i = 0
     for urls in results:
-        f = open("%s/results%i.csv" % (results_folder, i), "w+")
+        f = open("%s/results%i.tsv" % (results_folder, i), "w+")
         for url in urls:
             f.write("%s\r\n" % url)
         f.close()
         i += 1
-            
 
+def repair(results_filename, input_filename):
+    # Here's the program
+    # 1: Find all the URLs in input that have a comma and split them at their first comma
+    # 2: Loop through URLs in results and remove any that start like the URLs we found in step 1
+    # 3: Write the list generated in step 2 to a new file with tab separators
+    comma_urls = set()
+
+    # Step 1
+    f = open(input_filename, "r")
+    url = f.readline()
+    while(url):
+        url = url.strip()
+        comma_index = url.find(",")
+        if comma_index > 0:
+            comma_urls.add(url[:comma_index])
+        
+        url = f.readline()
+    f.close()
+
+    print("I found " + len(comma_urls) + " urls with commas.")
+
+    # Steps 2 and 3
+    new_results = []
+    f = open(results_filename, "r")
+    line = f.readline()
+    while(line):
+        comma_url = line.split(",")[0] # POTENTIAL comma URL
+        if comma_url in comma_urls:
+            print(comma_url + " is a comma URL")
+        else:
+            new_results_file.append(line)
+    f.close()
+
+    f = open("fixed.tsv", "w+")
+    for line in reader(new_results)
+        f.write("\t".join(line) + "\r\n")
+    f.close()
