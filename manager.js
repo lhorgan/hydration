1e1e65cca8f6b52e8d6734e2c8b13790230d7ea3
Luke Horgan <horgan.l@husky.neu.edu>
2019-10-29 20:23:51 -0400

    well, this is awful, but it works

---
+++ manager.js
@@ -0,0 +1,18 @@
+const child_process = require('child_process');
+
+function runner() {
+    var hydrater = child_process.spawn("node", ["hmain.js", "--max-old-space-size=12000"]);
+
+    hydrater.stdout.on('data', function(data) {
+        console.log(data.toString()); 
+    });
+
+    setTimeout(() => {
+        console.log("KILLING");
+        hydrater.stdin.pause();
+        hydrater.kill();
+        runner();
+    }, 15*60*1000);
+}
+
+runner();
